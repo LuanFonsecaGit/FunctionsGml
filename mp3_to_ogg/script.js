38c2cb2acf1385dac5c5ad296eec96a93f539c76
LanF <115931321+LuanFonsecaGit@users.noreply.github.com>
2024-06-01 20:43:28 +0100

    Update script.js

--- mp3_to_ogg/script.js
+++ mp3_to_ogg/script.js
@@ -14,7 +14,7 @@ document.getElementById('uploadForm').addEventListener('submit', function (event
     fetch('https://api.cloudconvert.com/v2/convert', {
         method: 'POST',
         headers: {
-            'Authorization': 'Bearer YOUR_API_KEY'
+            'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZTBjZjYzMmY2NTFmN2IwYjBiYjI0N2JiZGM1MTAxZDhjMWJlZTU1ZDBmOTg3ZGExODI0NTkxMjNmZmM2ODgyMGM3NDNhNmIxMTFjZmEzNjIiLCJpYXQiOjE3MTcyNzA5ODUuNTA1MjY2LCJuYmYiOjE3MTcyNzA5ODUuNTA1MjY5LCJleHAiOjQ4NzI5NDQ1ODUuNDk5NjYsInN1YiI6IjY4NTczNTM2Iiwic2NvcGVzIjpbXX0.HVXdHKkyshKqVj2vM93sEYJ2cu5C0_s3Sw8Q0jbdr9UZ5cY4bzDUCiFDLnZUf4YRIxbjeCe_RlqG9duU8CwZIUrWZ2y_4yV77lWGYG_eVqEMQeiMIFRlwetdR6FugzivBQU8btBPtSjCyJWQo88d7jxTfQChscuTSjPdr9IJ124Ok8U6YP4uvJliYU4KPJyNp-R3sPjC8fbFn_8lMzanUd1uSSI-k-o8r5oWhZ4ieUsbCa_Kg5wgam2jBc721WdfeXnUB4r-D0wAtFUTVthytRUKLIP4jK9SgqOcAWRng9ZcvDTRdRhKbHjKKHpw2r52A9WFcgdizObGXIhu0XSHUo_1f74mlc7c1zzy6LJ-tEEhGj2HhfNTVJdm1jrsWaWBgB5bqkHz8s9q0c8iv_Doolrt-NkzJvAXJXmJIGPeHOiDePf-NLaP_Ymjl5ZInYfuV5WJKuBVm5BczEpaQim_vYDSY97_F7MZC7zFLJ3JfwIYRgpZzok5wOlnwLBFyYoMDfh8M1J9z6aIZpcGkK2Ovc4ZpBLv207cLvuDG5FeDCPQ_bPjV-JfD_bRlwWSIP06fg-GjBi5M2PBLcR-eulgUYBcCdEymdA7LThyQqcEuPptucU4XufisPp1P7_2PtdNcF-iM0rLD7HkC0XmNYW1RCnUE-DjaYinLMfg2dbCswA'
         },
         body: formData
     })
